6a75b139de2d6e60cf70100ceaa70b6ff0296a54
Marcus Sera <m.yoshiosera@gmail.com>
2020-04-27 00:00:03 -0700

    added routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {Suspense, lazy} from "react";
 import SearchBar from "./SearchBar";
 import "./style.css";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+const Home = lazy(() => import('./Home'));
+const About = lazy(() => import('./About'));
 
 const App = () => {
   return (
     <div className="ui container">
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/" component={About}/>
+        </Switch>
+      </Suspense>
+    </Router>
       <SearchBar />
     </div>
   );
