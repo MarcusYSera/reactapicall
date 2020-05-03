71b17e2b5add43cc1914063c4bdce9731128bd2a
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-02 23:36:01 -0700

    lifecycle

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+  componentDidMount() {
+    console.log(this.imageRef);
+  }
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
         ImageCard
-        <img alt={description} src={urls.regular} />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
