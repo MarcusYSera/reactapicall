6df28fac7558df02aed54a16796df78ff6241357
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-03 22:14:03 -0700

    adding outside component

--- src/components/blog/BlogPost.js
+++ src/components/blog/BlogPost.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import faker from 'faker';
 
-const BlogPost = props => {
+const BlogPost = (props) => {
   return (
-    <div>
-      <a href="/" className="imate">
+    <div className="">
+      <a href="/" className="image">
         <img alt="user" src={faker.image.avatar()} />
       </a>
+      <div className="content">
+        <a href="/" className="">
+          {props.author}
+        </a>
+        <div className="">
+          <span>{props.timeAgo}</span>
+        </div>
+        <div className="body">Body of post</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default BlogPost;
