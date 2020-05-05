16510e217b42ff2e11309d65a7fed49ebc1190b6
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-04 22:01:35 -0700

    initiating props

--- src/components/video/VideoCard.js
+++ src/components/video/VideoCard.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const VideoCard = () => {
-  return (
-    <div>
-      <div>VideoCard</div>
-    </div>
-  );
-};
+class VideoCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { holder: 0 };
+  }
+  render() {
+    return (
+      <div>
+        <div>VideoCard</div>
+      </div>
+    );
+  }
+}
 
 export default VideoCard;
