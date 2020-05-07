b6e35d04700d3de171c22dc2cf6a529be6db9783
Marcus Sera <m.yoshiosera@gmail.com>
2020-05-06 23:38:27 -0700

    hierarchy

--- src/components/video/VideoList.js
+++ src/components/video/VideoList.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import VideoCard from './VideoCard';
 
 const VideoList = () => {
   return (
     <div>
       <div>Video List</div>
+      <VideoCard />
     </div>
   );
 };
